599ae4fb3bce034f59dbd2a72ece2e444999704c
tuyisenge250 <brnjaminwell250@gmail.com>
2024-11-14 21:25:17 +0200

    basket game

---
+++ front/html_Css/solo_basket_board/index.js
@@ -0,0 +1,14 @@
+let paraZero = document.getElementById("mark")
+let count = 0
+function btn1(){
+    count += 1;
+    paraZero.textContent = count; 
+}
+function btn2(){
+    count += 2;
+    paraZero.textContent = count; 
+}
+function btn3(){
+    count += 3;
+    paraZero.textContent = count; 
+}
